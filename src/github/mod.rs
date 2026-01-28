ddec706b2498ba09a3d238ab2f29da54c34825ba
dcouch440 <67534250+dcouch440@users.noreply.github.com>
2026-01-27 21:35:34 -0800

    8.{6,7}

--- src/github/mod.rs
+++ src/github/mod.rs
@@ -4,6 +4,8 @@ mod auth;
 mod client;
 mod comments;
 mod issue_sync;
+mod merge;
+mod merge_queue;
 mod pr;
 mod types;
 
@@ -11,6 +13,14 @@ pub use auth::{AuthError, DeviceCodeResponse, GitHubAuth, GitHubUser};
 pub use client::GitHubClient;
 pub use comments::{CommentError, CommentService, ProgressSummary, SliceProgress};
 pub use issue_sync::{convert_issue_to_ticket, IssueRef, IssueSync, IssueSyncError, SyncResult};
+pub use merge::{
+    MergeError, MergeMethod, MergePrRequest, MergePrResponse, MergePrResult, MergeableStatus,
+    RepoMergeSettings,
+};
+pub use merge_queue::{
+    ConflictInfoJson, MergeQueue, MergeQueueProcessor, NotificationOptions, ProcessResult,
+    PrQueueEntry, QueueError, QueueStats, QueueStatus,
+};
 pub use pr::{PrBodyGenerator, PrError, PrResult, PrService};
 pub use types::{
     CreateIssueComment, CreatePullRequest, CreateReviewRequest, FileStatus, GitHubApiUser,
