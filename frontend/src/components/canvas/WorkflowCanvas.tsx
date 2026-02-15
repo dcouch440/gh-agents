3d0fda7f1a36d51dc3b6cbbc8a67c5e2907a2033
David <dcouch440@gmail.com>
2026-02-15 11:46:01 -0800

    chore(canvas): reduce min zoom from 0.1x to 0.25x

--- frontend/src/components/canvas/WorkflowCanvas.tsx
+++ frontend/src/components/canvas/WorkflowCanvas.tsx
@@ -264,7 +264,7 @@ function WorkflowCanvasInner() {
         deleteKeyCode={['Backspace', 'Delete']}
         multiSelectionKeyCode="Shift"
         reconnectRadius={20}
-        minZoom={0.1}
+        minZoom={0.25}
         maxZoom={4}
         snapToGrid
         snapGrid={[CANVAS.GRID_SIZE, CANVAS.GRID_SIZE]}
